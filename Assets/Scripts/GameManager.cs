1083a7615724d377aa9aef050bc229aa0ebf50a3
clxrffdman <clxrffdman@gmail.com>
2023-02-05 14:59:35 -0800

    -added time slowdown

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : UnitySingleton<GameManager>
 {
+    public bool isSlowMo;
+    public IEnumerator SlowMo()
+    {
+        isSlowMo = true;
+        LeanTween.value(gameObject, 1, 0, 4).setIgnoreTimeScale(true).setOnUpdate((float val) => { Time.timeScale = val; });
+
+        yield return new WaitForSecondsRealtime(5);
+
+        LeanTween.value(gameObject, 0, 1, 1).setIgnoreTimeScale(true).setOnUpdate((float val) => { Time.timeScale = val; });
+        yield return new WaitForSecondsRealtime(1);
+
+        isSlowMo = false;
+    }
+
     public void WinGame()
     {
-        
+        SceneManager.LoadScene(0);
     }
+
+
 }
